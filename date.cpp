09fc2ac49ff46c90c0c400a291c605bd40fd775d
synrisea <150649087+synrisea@users.noreply.github.com>
2023-11-25 18:20:18 +0400

    Class
    Adding a class and methods, which are helping to count days in months.

--- date.cpp
+++ date.cpp
@@ -1,5 +1,139 @@
 #include <iostream>
 
+
+enum MonthOrder
+{
+    JANUARY = 1,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+};
+
+
+class Date
+{
+private:
+    int years = 0;
+    int months = 0;
+    int days = 0;
+
+
+    // Count the number of days in the current and previous months.
+
+    int countDaysInMonth(int& daysInMonth, int& daysInPrevMonth, int currentMonth) const
+    {
+        currentMonth = (currentMonth - 1) % 12 + 1;
+
+        switch (currentMonth)
+        {
+        case JANUARY:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case FEBRUARY:
+        {
+            if (!isLeapYear())
+            {
+                daysInMonth = 28;
+            }
+            else
+            {
+                daysInMonth = 29;
+            }
+
+            daysInPrevMonth = 31;
+            break;
+        }
+        case MARCH:
+        {
+            daysInMonth = 31;
+
+            if (!isLeapYear())
+            {
+                daysInPrevMonth = 28;
+            }
+            else
+            {
+                daysInPrevMonth = 29;
+            }
+            break;
+        }
+        case APRIL:
+        {
+            daysInMonth = 30;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case MAY:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 30;
+            break;
+        }
+        case JUNE:
+        {
+            daysInMonth = 30;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case JULY:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 30;
+            break;
+        }
+        case AUGUST:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case SEPTEMBER:
+        {
+            daysInMonth = 30;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case OCTOBER:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 30;
+            break;
+        }
+        case NOVEMBER:
+        {
+            daysInMonth = 30;
+            daysInPrevMonth = 31;
+            break;
+        }
+        case DECEMBER:
+        {
+            daysInMonth = 31;
+            daysInPrevMonth = 30;
+            break;
+        }
+        }
+
+        return daysInMonth;
+    }
+
+    bool isLeapYear() const
+    {
+        return (years % 4 == 0 && years % 100 != 0) || (years % 400 == 0);
+    }
+};
+
+
 int main()
 {
 
